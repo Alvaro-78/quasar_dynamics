72930954c1ba31c702d4a4cc4775f2c5fa5d71a4
Alvaro-78 <alvaro78an@gmail.com>
2023-12-18 19:41:10 +0100

    Show users in dashboard component

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -3,16 +3,8 @@ import { LoginService } from 'src/app/services/auth/login/login.component';
 
 @Component({
   selector: 'app-user-list',
-  template: `
-    <div *ngIf="isLoggedIn">
-      <h2>Lista de Usuarios</h2>
-      <ul>
-        <li *ngFor="let user of users">
-          {{ user.first_name }} {{ user.last_name }}
-        </li>
-      </ul>
-    </div>
-  `,
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
   isLoggedIn = false;
