f99a758b126473bc5c2e793c9b76aba7746b77dc
Alvaro-78 <alvaro78an@gmail.com>
2023-12-18 17:56:05 +0100

    Added login logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/auth/login/login.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginError: string = '';
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private loginService: LoginService
+  ) {}
+
+  // Simplifica los getters
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const email = this.f.email.value ?? '';
+      const password = this.f.password.value ?? '';
+
+      this.loginService.login(email, password).subscribe({
+        next: (userData) => {
+          console.log(userData);
+        },
+        error: (errorData) => {
+          console.error(errorData);
+          this.loginError = 'Email o password no válidos';
+        },
+        complete: () => {
+          console.log('Login completo');
+          this.router.navigateByUrl('/inicio');
+          this.loginForm.reset();
+        },
+      });
+    } else {
+      this.loginForm.markAllAsTouched();
+      alert('Faltan datos');
+    }
+  }
 }
