cca18bb7a8c70407b24aca11937f4fb982ce149f
Alvaro-78 <alvaro78an@gmail.com>
2023-12-18 17:54:30 +0100

    Added nav

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login/login.component';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
+  userLoginOn: boolean = false;
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.loginService.currentUserLogin.subscribe({
+      next: (userLoginOn) => {
+        this.userLoginOn = userLoginOn;
+      },
+    });
+  }
 }
