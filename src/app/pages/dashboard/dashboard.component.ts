709c0ca87774cb1178d2c564a10a79b7ad398d8a
Alvaro-78 <alvaro78an@gmail.com>
2023-12-18 17:56:51 +0100

    Added dashboard logic

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login/login.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
+  userLoginOn: boolean = true;
+  users: any[] = [];
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.loginService.currentUserLogin.subscribe((userLoginOn) => {
+      console.log('Estado de inicio de sesión:', userLoginOn);
+      this.userLoginOn = userLoginOn;
+      if (userLoginOn) {
+        this.loadUsers(1);
+        this.loadUsers(2);
+      }
+    });
+  }
+
+  private loadUsers(page: number) {
+    this.loginService.getUsers(page).subscribe(
+      (data) => {
+        this.users = this.users.concat(data.data); // Concatena los usuarios de cada página
+      },
+      (error) => {
+        console.error('Error al cargar usuarios', error);
+      }
+    );
+  }
 }
