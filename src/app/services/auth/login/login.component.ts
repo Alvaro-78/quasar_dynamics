92ffcc0f70b7cfc08eaa2d4659ff9fe1709c1d10
Alvaro-78 <alvaro78an@gmail.com>
2023-12-18 17:56:26 +0100

    Created login.service

---
+++ src/app/services/auth/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError, BehaviorSubject, tap } from 'rxjs';
+import { User } from '../../user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  currentUserLogin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  currentUserData: BehaviorSubject<User> = new BehaviorSubject<User>({
+    email: '',
+    password: '',
+  });
+
+  private apiUrl = 'https://reqres.in/api/login';
+  private usersUrl = 'https://reqres.in/api/users';
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.post<User>(this.apiUrl, { email, password }).pipe(
+      tap((userData: User) => {
+        this.currentUserData.next(userData);
+        this.currentUserLogin.next(true);
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('Algo ha fallado', error.error);
+    } else {
+      console.error(
+        'reqres.in ha retornado un error',
+        error.status,
+        error.error
+      );
+    }
+    return throwError(() => new Error(error.message));
+  }
+
+  get userData(): Observable<boolean> {
+    return this.currentUserLogin.asObservable();
+  }
+
+  getUsers(page: number = 1): Observable<any> {
+    const url = `${this.usersUrl}?page=${page}`;
+    return this.http.get<any>(url).pipe(catchError(this.handleError));
+  }
+
+  getUserById(userId: string): Observable<User> {
+    const url = `https://reqres.in/api/users/${userId}`;
+    return this.http.get<User>(url).pipe(catchError(this.handleError));
+  }
+}
